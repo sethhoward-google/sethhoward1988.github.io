780713b46d5cac963dd3c4b4235a596f544f7033
Seth Howard <smiles.seth@gmail.com>
2014-01-31 09:41:24 -0700

    event sourcing...

--- _site/js/main.js
+++ _site/js/main.js
@@ -7,3 +7,29 @@ $(function(){
 		}
 	})
 })
+
+
+
+
+// var exampleSocket = new WebSocket("");
+
+var source = new EventSource('http://67.222.234.99:20060/api/subscribe?source=chat&key=abcf219028cde834228888e01d014ae5e5abb12a36c904e78749972cdf17fdca');
+
+// exampleSocket.onmessage = function (event) {
+//   console.log(event);
+// }
+
+
+source.addEventListener('message', function(e) {
+  console.log(e);
+}, false);
+
+source.addEventListener('open', function(e) {
+  // Connection was opened.
+}, false);
+
+source.addEventListener('error', function(e) {
+  if (e.readyState == EventSource.CLOSED) {
+    // Connection was closed.
+  }
+}, false);
